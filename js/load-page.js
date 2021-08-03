ca42ed83518f21643fe348ce69f08ad3427db441
Adriano Oliveira Castro <56028189+aocastro@users.noreply.github.com>
2021-08-03 15:30:46 +0000

    03/08 - Continuação da introdução ao jQuery

--- js/load-page.js
+++ js/load-page.js
@@ -5,6 +5,27 @@ $(document).ready(function(){ //Executou a leitura do nosso documento e o prepar
   $('a').click(function(e){
     e.preventDefault()
 
+    let pagina = $(this).attr('href')
+
+    $('.modal-title').empty() 
+    $('.modal-body').empty() 
+
+    switch(pagina){
+      case 'projetos': 
+        $('.modal-title').append('Meus Projetos') 
+        $('.modal-body').append('<p>Projetos ...........</p>') 
+        break
+      case 'parceiros':
+        $('.modal-title').append('Meus Clientes/Parceiros') 
+        $('.modal-body').append('<p>Clientes ...........</p>') 
+        break
+      case 'contatos':
+        $('.modal-title').append('Meus Contatos') 
+        $('.modal-body').append('<p>Contatos ...........</p>') 
+        break
+      default: alert('Página não encontrada')
+    }
+
     $('#modal-info').modal('show')
 
   })
