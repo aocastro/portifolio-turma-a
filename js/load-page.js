73f840019a65a2407f0d4c34a3a5f41960a44a21
Adriano Oliveira Castro <56028189+aocastro@users.noreply.github.com>
2021-08-09 12:40:07 +0000

    09/08 - Personalização background via jQuery

--- js/load-page.js
+++ js/load-page.js
@@ -13,7 +13,19 @@ $(document).ready(function(){ //Executou a leitura do nosso documento e o prepar
     switch(pagina){
       case 'projetos': 
         $('.modal-title').append('Meus Projetos') 
-        $('.modal-body').append('<p>Projetos ...........</p>') 
+        $('.modal-body').append(`
+          <div class="container">
+            <div class="row">
+              <div class="col-12 col-sm-4 col-md-4">
+                <img src="img/projetos.jpeg" alt="Projetos" class="img-fluid">
+              </div>
+              <div class="col-12 col-sm-8 col-md-8">
+                <h4 class="text-center text-danger">Meus Projetos</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, eos asperiores ipsam et, natus quod nihil, iusto animi saepe hic adipisci beatae deleniti. Recusandae, quo. Deserunt ratione corrupti et recusandae!</p>
+              </div>
+            </div>
+          </div>
+        `) 
         break
       case 'parceiros':
         $('.modal-title').append('Meus Clientes/Parceiros') 
